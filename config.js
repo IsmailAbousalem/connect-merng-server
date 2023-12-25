c01e106f9f41735fa70e83d2870267bf9ade0e12
Ismail <ia.abousalem@gmail.com>
2023-12-24 22:01:35 -0500

    fixed config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB: 
+        'mongodb+srv://Ismail:Ismail2002@cluster0.ovd1pnk.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'if you see this, hello'
+};
+
